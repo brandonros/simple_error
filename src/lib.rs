7e75fd829f375b0ceda1542721cee53b5cdcb8c1
Brandon Ros <brandonros1@gmail.com>
2024-10-19 10:55:35 -0400

    box_err

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,8 @@
-use std::error::Error;
+pub type SimpleResult<R> = Result<R, Box<dyn std::error::Error + Send + Sync>>;
 
-pub type SimpleResult<R> = Result<R, Box<dyn Error + Send + Sync>>;
+#[macro_export]
+macro_rules! box_err {
+    ($msg:expr) => {
+        Box::<dyn std::error::Error + Send + Sync>::from($msg)
+    };
+}
